7da63e6b0c3f23f092199e67cf9d0cf1855e3f1b
Bravo1B9 <b1@xgmi.com>
2023-02-16 05:40:19 +0000

    Implement get me functionality

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { Request } from "express";
 
 @Controller('users')
 export class UserController {
+
+  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe() {
-    return { info: 'User info' }
+  getMe(@Req() req: Request) {
+    console.log({
+      user: req.user
+    });
+    return req.user;
   }
+
 }
